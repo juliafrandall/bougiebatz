6d760317666811d5a462ab561894c14257e58425
Janis Randall <juliafrandall@gmail.com>
2016-12-21 16:06:45 -0500

    merge

--- client/components/Photoentry.js
+++ client/components/Photoentry.js
@@ -4,15 +4,15 @@ import { default as Fade } from 'react-fade';
 //creating template f
 
 var PhotoEntry = ({photos, handleSearchParamChange}) => (
-	<div className="smallPhotos">
-	{photos.map((photo, i) =>
-		<Fade duration={.8}>
+  <div className="smallPhotos">
+  {photos.map((photo, i) =>
+    <Fade duration={.8}>
      <div>
        <a href={photo.url}>
          <img className="smallPhotoItem" src={photo.multimedia[2].url} />
        </a>
     </div>
-		</Fade>
+    </Fade>
    )}
  </div>
 )
